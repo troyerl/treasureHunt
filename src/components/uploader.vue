83b0e51d6ffc202e374fec34a3a9490f8a190641
Logan Troyer <troyerlogan@gmail.com>
2020-06-22 13:56:19 -0400

    Init commit

---
+++ src/components/uploader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <b-form-file v-model="file2" class="mt-3" plain></b-form-file>
+    <div class="mt-3">Selected file: {{ file2 ? file2.name : '' }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      file: null,
+      file2: null
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/router.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import VideoPlayer from './components/videoPlayer';
+import Uploader from './components/uploader';
+
+Vue.use(Router);
+
+const router = new Router({
+  mode: 'history',
+  routes: [
+      {
+          path: '*',
+          redirect: '/'
+      },
+      {
+        path: '/',
+        component: Uploader
+      },
+      {
+          path: '/video',
+          component: VideoPlayer,
+      }
+  ]
+});
+
+export default router;
